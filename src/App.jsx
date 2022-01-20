4eb6179128c47265bf06d0e4e9e5b71f005586b4
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2022-01-20 19:49:59 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllTopics, getAllTopicsDetails } from './features/actions';
 import FooterBox from './components/FooterBox/FooterBox';
-import FooterTop from './components/FooterTop/FooterTop';
+import FooterTop from './components/Footertop/FooterTop';
 function App() {
     const { allTopics, allTopicsDetails } = useSelector((state) => state);
     const dispatch = useDispatch();
