f1150c273164eb3cbdfc79ccb8a241e982411c1e
AvdheshPal <87435192+AvdheshPal@users.noreply.github.com>
2022-01-20 22:45:54 +0530

    Created Profile Page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getAllTopics, getAllTopicsDetails } from './features/actions';
 import TopicCard from './components/TopicCard/TopicCard';
 import AllTopics from './components/AllTopics/AllTopics';
+import {Profile} from './components/Profile/Profile'
 
 function App() {
     const { allTopics, allTopicsDetails } = useSelector((state) => state);
@@ -18,6 +19,7 @@ function App() {
     return (
         <div className='App'>
             <AllTopics />
+            {/* <Profile/> */}
         </div>
     );
 }
