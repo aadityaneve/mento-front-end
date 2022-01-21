b5fcb162e0ae33a231568823acca24a7a1d14e03
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2022-01-21 18:04:33 +0530

    upadate

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import { useState, useEffect } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllTopics, getAllTopicsDetails } from './features/actions';
-import Midpart from './components/Midpart/Midpart';
-import AllTopics from './components/AllTopics/AllTopics';
-import Midtextbox from './components/Midtextbox/Midtextbox';
-import Videobox from './components/Videobox/Videobox';
+// import Midpart from './components/Midpart/Midpart';
+// import AllTopics from './components/AllTopics/AllTopics';
+// import Midtextbox from './components/Midtextbox/Midtextbox';
+// import Videobox from './components/Videobox/Videobox';
 
 function App() {
     const { allTopics, allTopicsDetails } = useSelector((state) => state);
