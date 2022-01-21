8e4d5b93b140273378a52d782b940b82cbc9c6de
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2022-01-21 21:33:35 +0530

    Homepage Created

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import FooterTop from '../Footer/FooterTop';
+import FooterBottom from '../Footer/FooterBottom';
+function Home() {
+  return <>
+    <Navbar />
+    
+    <FooterTop />
+    <FooterBottom />
+  </>;
+}
+
+export default Home;
